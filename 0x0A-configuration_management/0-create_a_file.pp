7bfba2658a7edd981f4f61526e6e5cd883c1a5c6
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-09-22 21:45:22 +0300

    fix:create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
+# creates a file
+ 
 file { '/tmp/school':
-  mode => '0744'
-  owner => 'www-data'
-  group => 'www-data'
-  content => 'I love Puppet'
+  ensure  => file,
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
