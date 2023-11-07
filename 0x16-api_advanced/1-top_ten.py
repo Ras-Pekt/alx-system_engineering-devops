e5fc164b844e151546007fa8c40ae72a65b8e438
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-11-07 14:24:29 +0300

    fix:get first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.12) \
             Gecko/20080214 Firefox/2.0.0.12",
