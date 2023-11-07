9021cd1a4d26e7bf68ed0323adbe502acad9eb23
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-11-07 14:08:40 +0300

    get first 10 hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed
+for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.12) \
+            Gecko/20080214 Firefox/2.0.0.12",
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        for key in response.json().get("data").get("children"):
+            print(f"{key.get('data').get('title')}")
+    else:
+        print("None")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,3 +1,4 @@
 # 0x16. API advanced
 This directory contains scripts that return data from Reddit's API:
-- [0-subs.py](0-subs.py) - a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+- [0-subs.py](0-subs.py) - queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit
+- [1-top_ten.py](1-top_ten.py) - queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
