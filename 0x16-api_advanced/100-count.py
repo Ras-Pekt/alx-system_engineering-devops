1a92d0caec7b5a569c33c57894f06cb2939ad6bf
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-11-09 22:24:58 +0300

    count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+a recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list, after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.12) \
+            Gecko/20080214 Firefox/2.0.0.12",
+    }
+
+    if after is not None:
+        url += f"?after={after}"
+
+    response = requests.get(url, headers=headers)
+
+    data = response.json().get('data', {})
+
+    posts = data.get('children', None)
+    if posts is None:
+        return None
+
+    for post in posts:
+        word_list.append(post.get('data').get('title'))
+
+    after = data.get('after')
+    if after is None:
+        return word_list
+    return count_words(subreddit, word_list, after)
